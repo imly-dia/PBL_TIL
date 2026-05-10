ff8979d9d5ae1cefa21ab0cdc233c24415d13774
imly-dia <imlyx.xdia@gmail.com>
2026-05-10 17:32:43 +0900

    Add files via upload

---
+++ week05/src/lions.js
@@ -0,0 +1,30 @@
+export const LIONS_DATA = [
+    {
+      id: "me",
+      name: "조윤아",
+      profileImg: "/IMG_2136.jpeg", // 이미지는 public 폴더에 넣어줘!
+      part: "Frontend",
+      skills: ["React", "TypeScript", "CSS"],
+      oneLine: "안녕하세요 14기 아기사자 조윤아입니다!",
+      bio: "반갑습니다! 정보통신공학과 조윤아입니다.",
+      email: "box7431@naver.com",
+      phone: "010-4564-0000",
+      website: "https://www.instagram.com/imly._.dia",
+      resolution: "열정을 가진 팀원들과 함께 1년 동안 열심히 하겠습니다!",
+      isMe: true
+    },
+    {
+      id: "lion2",
+      name: "멋쟁이 사자",
+      profileImg: "/고양이2.jpg",
+      part: "Backend",
+      skills: ["Node.js", "Java", "Python"],
+      oneLine: "코딩을 사랑하는 백엔드 아기사자입니다.",
+      bio: "안녕하세요! 서버 로직 설계를 즐깁니다.",
+      email: "lion@hufs.ac.kr",
+      phone: "010-1234-5678",
+      website: "https://github.com",
+      resolution: "최고의 개발자가 되겠습니다!",
+      isMe: false
+    }
+  ];
